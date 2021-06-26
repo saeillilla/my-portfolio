2b62e6605687273ad1982aad21fdb22f4230706d
saeillilla <saeil.moorsingal@gmail.com>
2021-06-26 05:31:25 +0530

    first commit

---
+++ src/client.js
@@ -0,0 +1,5 @@
+import { sanityClient } from "@sanity/client";
+export default sanityClient({
+    projectId:"vc0ochrt",
+    dataset:"production"
+})
---
+++ src/components/Post.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Post(){
+    return <h1>Post Page</h1>
+}
